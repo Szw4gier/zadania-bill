4858975ebba0b6a01b495ef8cf750f35692ea8ab
Mateusz Domjan <mateusz.domjan@billennium.com>
2019-02-21 13:58:25 +0100

    zad2 before refactor

--- zad 2/choinka.js
+++ zad 2/choinka.js
@@ -1,1 +1,46 @@
 console.log("choinka.js");
+
+let tree = document.getElementById('tree');
+const levelCounter = document.getElementById('levelsAmount');
+
+document.getElementById('generate')
+    .addEventListener("click", displayTree);
+
+const initTree = () => {
+    const levels = document.getElementById('levels').value;
+    const levelCapacity = document.getElementById('levelCapacity').value;
+
+    levelCounter.innerHTML = getLevelsAmount(levels,levelCapacity);
+    drawTree(levels,levelCapacity);
+}
+
+const getLevelsAmount = (levels, levelCapacity) => {
+    return Math.ceil(levels/levelCapacity);
+}
+
+const drawTree  = (levels, levelCapacity) => {
+    const starNode = document.createElement('div');
+    const star = document.createTextNode('*');
+    starNode.appendChild(star);
+    tree.appendChild(starNode);
+
+    let count = 0;
+    const whiteSpaces = [];
+
+    while( count < levels ) {
+        if ( count % levelCapacity !== 0) {
+            whiteSpaces.push(' ');
+        }
+
+        const branchNode = document.createElement('div'); 
+        const branch = document.createTextNode('/' + whiteSpaces + '\\');
+        branchNode.appendChild(branch);
+        tree.appendChild(branchNode);
+
+        count++;
+    }
+}
+
+function displayTree() {
+    initTree();
+}
