747ddb65c1262357d334b6d4ed3f4018969a21a9
Mateusz Domjan <mateusz.domjan@billennium.com>
2019-02-21 12:07:37 +0100

    zad1 a completed

--- zad 1/tajemnicza-tablica.js
+++ zad 1/tajemnicza-tablica.js
@@ -1,1 +1,31 @@
 console.log("tajemnicza-tablica.js")
+
+let letters = document.getElementsByTagName('tbody');
+
+const findHiddenLetters = (elements) => {
+    const password = [];
+
+    const rows = elements[0].children;
+    for (let row of rows) {
+        for (let letter of row.children) {
+            if (letter.style.backgroundColor === letter.style.color) {
+                password.push(letter.textContent);
+            }
+        }
+    }
+
+    return password;
+}
+
+const password = findHiddenLetters(letters);
+
+const passToString = password.join().replace(/,/g, '');
+
+const passwordField = document.getElementById('result');
+
+function findPassword() {
+    passwordField.style.color = "red";
+    passwordField.innerHTML = passToString;
+}
+
+console.log(passToString);
